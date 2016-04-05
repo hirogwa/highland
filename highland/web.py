40ed2c805042f10ad9b4066e7b9e1a083948b1d3
hirogwa <hirogwa@gmail.com>
2016-04-05 21:22:51 +0900

    bootstrap logger

--- highland/web.py
+++ highland/web.py
@@ -4,6 +4,7 @@ from highland import app, models
 
 @app.route('/ping', methods=['GET'])
 def ping():
+    app.logger.debug('processing ping request')
     print(request.form)
     print(request.args)
     print(request.get_json())
--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from highland import app
+from logging.handlers import RotatingFileHandler
+import logging
 import os
+
+handler = RotatingFileHandler('highland.log', maxBytes=10000000, backupCount=1)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(logging.Formatter(
+    "%(asctime)s %(pathname)s:%(lineno)d %(levelname)s - %(message)s"))
+app.logger.addHandler(handler)
 
 app.debug = True
 app.run(
