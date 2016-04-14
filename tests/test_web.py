d1d03584b4cb85cf364dd75ada0fc8b1efb70851
hirogwa <hirogwa@gmail.com>
2016-04-14 22:03:10 +0900

    add unit test for web.py

---
+++ tests/test_web.py
@@ -0,0 +1,45 @@
+import unittest
+import highland
+from flask import json
+from unittest.mock import MagicMock
+from highland import show_operation, models
+
+
+class TestShow(unittest.TestCase):
+    def setUp(self):
+        highland.app.config['TESTING'] = True
+        self.app = highland.app.test_client()
+
+    def post_with_json(self, **kwargs):
+        return self.app.post('/show',
+                             data=json.dumps(kwargs),
+                             content_type='application/json')
+
+    @unittest.mock.patch.object(show_operation, 'create')
+    def test_show_post(self, mocked_create):
+        title = 'my title'
+        description = 'my description'
+
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        show = models.Show(mocked_user, title, description)
+        show.id = 2
+        mocked_create.return_value = show
+
+        response = self.post_with_json(title=title, description=description)
+
+        resp_data = json.loads(response.data)
+        resp_show = resp_data.get('show')
+        self.assertEqual('success', resp_data.get('result'))
+        self.assertEqual(show.owner_user_id, resp_show.get('owner_user_id'))
+        self.assertEqual(show.id, resp_show.get('id'))
+        self.assertEqual(show.title, resp_show.get('title'))
+        self.assertEqual(show.description, resp_show.get('description'))
+
+    def test_show_post_input_check_title(self):
+        with self.assertRaises(AssertionError):
+            self.post_with_json(description='my description')
+
+    def test_show_post_input_check_description(self):
+        with self.assertRaises(AssertionError):
+            self.post_with_json(title='my title')
