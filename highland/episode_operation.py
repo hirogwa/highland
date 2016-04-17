a1e1778f45b3896fc53d5397aa1cadf0eb11f098
hirogwa <hirogwa@gmail.com>
2016-04-17 17:06:22 +0900

    clean episode operation

--- highland/episode_operation.py
+++ highland/episode_operation.py
@@ -2,14 +2,8 @@ from highland import models
 
 
 def create(user, show_id, title, description, audio_id):
-    show = models.Show.query.\
-        filter_by(owner_user_id=user.id, id=show_id).first()
-    audio = models.Audio.query.\
-        filter_by(owner_user_id=user.id, id=audio_id).first()
-
-    assert show, 'No such show. (user,show)=({0},{1})'.format(user.id, show_id)
-    assert audio,\
-        'No such audio. (user,audio)=({0},{1})'.format(user.id, audio_id)
+    show = get_show_or_assert(user, show_id)
+    audio = get_audio_or_assert(user, audio_id)
 
     episode = models.Episode(show, title, description, audio)
     models.db.session.add(episode)
@@ -18,21 +12,9 @@ def create(user, show_id, title, description, audio_id):
 
 
 def update(user, show_id, episode_id, title, description, audio_id):
-    show = models.Show.query.\
-        filter_by(owner_user_id=user.id, id=show_id).first()
-    episode = models.Episode.query.\
-        filter_by(owner_user_id=user.id,
-                  show_id=show_id,
-                  id=episode_id).first()
-    audio = models.Audio.query.\
-        filter_by(owner_user_id=user.id, id=audio_id).first()
-
-    assert show, 'No such show. (user,show)=({0},{1})'.format(user.id, show_id)
-    assert episode,\
-        'No such episode. (user,show,episode)=({0},{1},{2})'.\
-        format(user.id, show_id, episode_id)
-    assert audio,\
-        'No such audio. (user,audio)=({0},{1})'.format(user.id, audio_id)
+    get_show_or_assert(user, show_id)
+    episode = get_episode_or_assert(user, show_id, episode_id)
+    audio = get_audio_or_assert(user, audio_id)
 
     episode.title = title
     episode.description = description
@@ -48,10 +30,40 @@ def delete(episode):
 
 
 def load(user, show_id):
-    show = models.Show.query.\
-        filter_by(owner_user_id=user.id, id=show_id).first()
-    assert show, 'No such show. (user,show)=({0},{1})'.format(user.id, show_id)
-
+    show = get_show_or_assert(user, show_id)
     return models.Episode.query.\
         filter_by(owner_user_id=show.owner_user_id, show_id=show.id).\
         all()
+
+
+def get_show_or_assert(user, show_id):
+    show = models.Show.query.\
+        filter_by(owner_user_id=user.id, id=show_id).first()
+    if show:
+        return show
+    else:
+        raise AssertionError(
+            'No such show. (user,show):({0},{1})'.format(user.id, show_id))
+
+
+def get_audio_or_assert(user, audio_id):
+    audio = models.Audio.query.\
+        filter_by(owner_user_id=user.id, id=audio_id).first()
+    if audio:
+        return audio
+    else:
+        raise AssertionError(
+            'No such audio. (user,audio)=({0},{1})'.format(user.id, audio_id))
+
+
+def get_episode_or_assert(user, show_id, episode_id):
+    episode = models.Episode.query.\
+        filter_by(owner_user_id=user.id,
+                  show_id=show_id,
+                  id=episode_id).first()
+    if episode:
+        return episode
+    else:
+        raise AssertionError(
+            'No such episode. (user,show,episode)=({0},{1},{2})'.
+            format(user.id, show_id, episode_id))
--- tests/test_episode_operation.py
+++ tests/test_episode_operation.py
@@ -4,40 +4,55 @@ from highland import episode_operation, models
 
 
 class TestEpisodeOperation(unittest.TestCase):
+    @unittest.mock.patch.object(episode_operation, 'get_show_or_assert')
+    @unittest.mock.patch.object(episode_operation, 'get_audio_or_assert')
     @unittest.mock.patch('highland.models.Episode')
     @unittest.mock.patch.object(models.db.session, 'commit')
     @unittest.mock.patch.object(models.db.session, 'add')
-    def test_create(self, mocked_add, mocked_commit, mocked_episode_class):
+    def test_create(self, mocked_add, mocked_commit, mocked_episode_class,
+                    mocked_get_audio, mocked_get_show):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
         mocked_show = MagicMock()
-        mocked_show.owner_user_id = 1
+        mocked_show.owner_user_id = mocked_user.id
         mocked_show.id = 2
-
         mocked_audio = MagicMock()
         mocked_audio.id = 3
 
         title = 'my episode'
         description = 'my episode description'
 
+        mocked_get_show.return_value = mocked_show
+        mocked_get_audio.return_value = mocked_audio
         mocked_episode = MagicMock()
         mocked_episode_class.return_value = mocked_episode
 
         result = episode_operation.create(
-            mocked_show, title, description, mocked_audio)
+            mocked_user, mocked_show.id, title, description, mocked_audio.id)
 
+        mocked_get_show.assert_called_with(mocked_user, mocked_show.id)
+        mocked_get_audio.assert_called_with(mocked_user, mocked_audio.id)
         mocked_episode_class.assert_called_with(
             mocked_show, title, description, mocked_audio)
         mocked_add.assert_called_with(mocked_episode)
         mocked_commit.assert_called_with()
         self.assertEqual(mocked_episode, result)
 
+    @unittest.mock.patch.object(episode_operation, 'get_audio_or_assert')
+    @unittest.mock.patch.object(episode_operation, 'get_episode_or_assert')
+    @unittest.mock.patch.object(episode_operation, 'get_show_or_assert')
     @unittest.mock.patch.object(models.db.session, 'commit')
-    def test_update(self, mocked_commit):
+    def test_update(self, mocked_commit,
+                    mocked_get_show, mocked_get_episode, mocked_get_audio):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+
         mocked_show = MagicMock()
-        mocked_show.owner_user_id = 1
-        mocked_show.id = 2
+        mocked_show.owner_user_id = 2
+        mocked_show.id = 3
 
         mocked_audio_original = MagicMock()
-        mocked_audio_original.id = 3
+        mocked_audio_original.id = 4
 
         mocked_episode = MagicMock()
         mocked_episode.owner_user_id = mocked_show.owner_user_id
@@ -51,9 +66,17 @@ class TestEpisodeOperation(unittest.TestCase):
         mocked_audio_new = MagicMock()
         mocked_audio_new.id = 11
 
-        result = episode_operation.update(
-            mocked_episode, title, description, mocked_audio_new)
+        mocked_get_episode.return_value = mocked_episode
+        mocked_get_audio.return_value = mocked_audio_new
 
+        result = episode_operation.update(
+            mocked_user, mocked_show.id, mocked_episode.id, title,
+            description, mocked_audio_new.id)
+
+        mocked_get_show.assert_called_with(mocked_user, mocked_show.id)
+        mocked_get_episode.assert_called_with(
+            mocked_user, mocked_show.id, mocked_episode.id)
+        mocked_get_audio.assert_called_with(mocked_user, mocked_audio_new.id)
         mocked_commit.assert_called_with()
         self.assertEqual(mocked_show.owner_user_id,
                          mocked_episode.owner_user_id)
@@ -81,14 +104,17 @@ class TestEpisodeOperation(unittest.TestCase):
         mocked_commit.assert_called_with()
         self.assertTrue(result)
 
+    @unittest.mock.patch.object(episode_operation, 'get_show_or_assert')
     @unittest.mock.patch('highland.models.Episode.query')
-    def test_load(self, mocked_query):
+    def test_load(self, mocked_query, mocked_get_show):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
         mocked_show = MagicMock()
-        mocked_show.owner_user_id = 1
+        mocked_show.owner_user_id = mocked_user.id
         mocked_show.id = 2
-
         mocked_audio = MagicMock()
         mocked_audio.id = 3
+        mocked_get_show.return_value = mocked_show
 
         ep_one = models.Episode(
             mocked_show, 'title one', 'desc one', mocked_audio)
@@ -100,10 +126,114 @@ class TestEpisodeOperation(unittest.TestCase):
         mocked_filter.all.return_value = ep_list
         mocked_query.filter_by.return_value = mocked_filter
 
-        result = episode_operation.load(mocked_show)
+        result = episode_operation.load(mocked_user, mocked_show.id)
 
         mocked_query.filter_by.assert_called_with(
             owner_user_id=mocked_show.owner_user_id,
             show_id=mocked_show.id)
         mocked_filter.all.assert_called_with()
         self.assertEqual(ep_list, result)
+
+    @unittest.mock.patch('highland.models.Show.query')
+    def test_get_show_or_assert(self, mocked_query):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        mocked_show = MagicMock()
+        mocked_show.owner_user_id = mocked_user.id
+        mocked_show.id = 2
+
+        mocked_filter = MagicMock()
+        mocked_filter.first.return_value = mocked_show
+        mocked_query.filter_by.return_value = mocked_filter
+
+        result = episode_operation.get_show_or_assert(
+            mocked_user, mocked_show.id)
+
+        mocked_query.filter_by.assert_called_with(owner_user_id=mocked_user.id,
+                                                  id=mocked_show.id)
+        mocked_filter.first.assert_called_with()
+        self.assertEqual(result, mocked_show)
+
+    @unittest.mock.patch('highland.models.Show.query')
+    def test_get_show_or_assert_assert(self, mocked_query):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        show_id = 2
+
+        mocked_filter = MagicMock()
+        mocked_filter.first.return_value = None
+        mocked_query.filter_by.return_value = mocked_filter
+
+        with self.assertRaises(AssertionError):
+            episode_operation.get_show_or_assert(
+                mocked_user, show_id)
+
+    @unittest.mock.patch('highland.models.Audio.query')
+    def test_get_audio_or_assert(self, mocked_query):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        mocked_audio = MagicMock()
+        mocked_audio.owner_user_id = mocked_user.id
+        mocked_audio.id = 2
+
+        mocked_filter = MagicMock()
+        mocked_filter.first.return_value = mocked_audio
+        mocked_query.filter_by.return_value = mocked_filter
+
+        result = episode_operation.get_audio_or_assert(
+            mocked_user, mocked_audio.id)
+
+        mocked_query.filter_by.assert_called_with(owner_user_id=mocked_user.id,
+                                                  id=mocked_audio.id)
+        mocked_filter.first.assert_called_with()
+        self.assertEqual(result, mocked_audio)
+
+    @unittest.mock.patch('highland.models.Audio.query')
+    def test_get_audio_or_assert_assert(self, mocked_query):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        audio_id = 2
+
+        mocked_filter = MagicMock()
+        mocked_filter.first.return_value = None
+        mocked_query.filter_by.return_value = mocked_filter
+
+        with self.assertRaises(AssertionError):
+            episode_operation.get_audio_or_assert(
+                mocked_user, audio_id)
+
+    @unittest.mock.patch('highland.models.Episode.query')
+    def test_get_episode_or_assert(self, mocked_query):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        show_id = 2
+        mocked_episode = MagicMock()
+        mocked_episode.id = 3
+
+        mocked_filter = MagicMock()
+        mocked_filter.first.return_value = mocked_episode
+        mocked_query.filter_by.return_value = mocked_filter
+
+        result = episode_operation.get_episode_or_assert(
+            mocked_user, show_id, mocked_episode.id)
+
+        mocked_query.filter_by.assert_called_with(owner_user_id=mocked_user.id,
+                                                  show_id=show_id,
+                                                  id=mocked_episode.id)
+        mocked_filter.first.assert_called_with()
+        self.assertEqual(result, mocked_episode)
+
+    @unittest.mock.patch('highland.models.Episode.query')
+    def test_get_episode_or_assert_assert(self, mocked_query):
+        mocked_user = MagicMock()
+        mocked_user.id = 1
+        show_id = 2
+        episode_id = 3
+
+        mocked_filter = MagicMock()
+        mocked_filter.first.return_value = None
+        mocked_query.filter_by.return_value = mocked_filter
+
+        with self.assertRaises(AssertionError):
+            episode_operation.get_episode_or_assert(
+                mocked_user, show_id, episode_id)
