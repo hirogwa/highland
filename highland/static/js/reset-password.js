2d5b885e0141a8f111f30431e311ecd93f285932
hirogwa <hirogwa@gmail.com>
2016-11-29 22:22:20 +0900

    Get some UI feedback upon password reset

--- highland/static/js/reset-password.js
+++ highland/static/js/reset-password.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
-import { PasswordResetInputs, TextInput } from "./common.js";
+import { AlertBox, PasswordResetInputs, TextInput } from "./common.js";
 
 class ResetPassword extends React.Component {
     constructor(props) {
@@ -16,11 +16,15 @@ class ResetPassword extends React.Component {
         this.state = {
             passwordCurrent: '',
             passwordNew: '',
-            passwordNewRetyped: ''
+            passwordNewRetyped: '',
+            activeAlert: null,
+            requestSent: false
         };
     }
 
     execute() {
+        this.setState({ requestSent: true });
+
         const userPool = new CognitoUserPool({
             UserPoolId: this.props.route.cognitoUserPoolId,
             ClientId: this.props.route.cognitoClientId
@@ -32,13 +36,32 @@ class ResetPassword extends React.Component {
             }
         });
 
+        const self = this;
         cognitoUser.changePassword(
             this.state.passwordCurrent,
             this.state.passwordNew,
             function(err) {
+                let content = '';
+                let style = '';
                 if (err) {
                     console.error(err);
+                    style = 'danger';
+                    if (err.code === "NotAuthorizedException") {
+                        content = 'Incorrect username or password. Please double check and try again.';
+                    } else {
+                        content = 'Password reset failed. Please try again.';
+                    }
+                } else {
+                    style = 'success';
+                    content = 'Password updated successfully.';
                 }
+                self.setState({
+                    passwordCurrent: '',
+                    passwordNew: '',
+                    passwordNewRetyped: '',
+                    activeAlert: { style: style, content: content },
+                    requestSent: false
+                });
             });
     }
 
@@ -61,22 +84,45 @@ class ResetPassword extends React.Component {
         this.setState({ passwordNewRetyped: text });
     }
 
+    buttonDisabled() {
+        if (!this.validInput()) {
+            return true;
+        }
+        return this.state.requestSent;
+    }
+
+    buttonText() {
+        return this.state.requestSent ? 'Reseting Password...' : 'Reset Password';
+    }
+
     render() {
+        let alertBox = <div></div>;
+        if (this.state.activeAlert) {
+            alertBox = (
+                <AlertBox style={this.state.activeAlert.style}
+                          content={this.state.activeAlert.content}
+                          nondismissible>
+                </AlertBox>
+            );
+        }
         return (
             <div className="container">
+              {alertBox}
               <TextInput name='Current password'
                          type='password'
                          autoFocus
                          value={this.state.passwordCurrent}
                          handleChange={this.handleChangePasswordCurrent} />
               <PasswordResetInputs
+                 password={this.state.passwordNew}
+                 passwordRetyped={this.state.passwordNewRetyped}
                  handleChangePassword={this.handleChangePasswordNew}
                  handleChangePasswordRetyped={this.handleChangePasswordNewRetyped} />
               <Button bsStyle="success"
                       className="pull-right"
                       onClick={this.execute}
-                      disabled={!this.validInput()}>
-                Reset Password
+                      disabled={this.buttonDisabled()}>
+                {this.buttonText()}
               </Button>
             </div>
         );
