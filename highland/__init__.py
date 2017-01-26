dd491c2ac8ad85b602f251dfc15a4415d83b564f
hirogwa <hirogwa@gmail.com>
2017-01-26 23:39:05 +0900

    avoid referring to settings directly

--- highland/__init__.py
+++ highland/__init__.py
@@ -1,7 +1,6 @@
 import flask
-from highland import settings
 app = flask.Flask(__name__)
 app.config.from_object('highland.settings_dev')
-app.config['SQLALCHEMY_DATABASE_URI'] = settings.DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config.get('DATABASE')
 
 import highland.web
