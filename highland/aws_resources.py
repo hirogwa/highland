5cd94cdcc58e46e0ca33f735f69b83e04963deef
hirogwa <hirogwa@gmail.com>
2017-01-26 22:14:12 +0900

    set aws keys at host level

--- highland/aws_resources.py
+++ highland/aws_resources.py
@@ -1,21 +1,6 @@
 import boto3
 from highland import app
 
-s3 = boto3.resource(
-    's3',
-    region_name=app.config.get('S3_REGION'),
-    aws_access_key_id=app.config.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=app.config.get('AWS_SECRET_ACCESS_KEY')
-)
-
-cognito_idp = boto3.client(
-    'cognito-idp',
-    aws_access_key_id=app.config.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=app.config.get('AWS_SECRET_ACCESS_KEY')
-)
-
-cognito_identity = boto3.client(
-    'cognito-identity',
-    aws_access_key_id=app.config.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=app.config.get('AWS_SECRET_ACCESS_KEY')
-)
+s3 = boto3.resource('s3', region_name=app.config.get('S3_REGION'))
+cognito_idp = boto3.client('cognito-idp')
+cognito_identity = boto3.client('cognito-identity')
