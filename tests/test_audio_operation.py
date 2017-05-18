2ca8373f9f42b3ead097ca906f5d6459885ea804
hirogwa <hirogwa@gmail.com>
2017-05-18 21:17:31 +0900

    Fix unit tests for media creation

--- tests/test_audio_operation.py
+++ tests/test_audio_operation.py
@@ -7,153 +7,146 @@ from highland import audio_operation, models, media_storage, settings, \
 class TestAudioOperation(unittest.TestCase):
     @patch.object(models.db.session, 'commit')
     @patch.object(models.db.session, 'add')
-    @patch('highland.models.Audio')
     @patch('uuid.uuid4')
-    def test_create(
-            self, mocked_uuid4, mocked_audio_class, mocked_add, mocked_commit):
-        mocked_user, mocked_audio, mocked_uuid = \
-            MagicMock(), MagicMock(), MagicMock()
-        mocked_uuid4.return_value = mocked_uuid
-        mocked_audio_class.return_value = mocked_audio
+    def test_create(self, mock_uuid4, mock_add, mock_commit):
+        mock_uuid4.return_value.hex = 'some_guid'
 
-        result = audio_operation.create(
-            mocked_user, 'some.mp3', 120, 6400, 'audio/mpeg')
+        result = audio_operation.create(1, 'some.mp3', 120, 6400, 'audio/mpeg')
 
-        self.assertEqual(1, mocked_add.call_count)
-        mocked_commit.assert_called_with()
-        mocked_audio_class.assert_called_with(
-            mocked_user, 'some.mp3', 120, 6400, 'audio/mpeg', mocked_uuid.hex)
-        self.assertEqual(mocked_audio, result)
+        self.assertEqual(1, mock_add.call_count)
+        mock_commit.assert_called_with()
+        self.assertEqual(1, result.get('owner_user_id'))
+        self.assertEqual('some_guid', result.get('guid'))
 
     @patch.object(models.db.session, 'commit')
     @patch.object(models.db.session, 'delete')
     @patch.object(media_storage, 'delete')
     @patch.object(audio_operation, 'get_audio_or_assert')
-    def test_delete(self, mocked_get_audio, mocked_media_delete,
-                    mocked_delete, mocked_commit):
-        mocked_audio, mocked_user = MagicMock(), MagicMock()
-        mocked_user.identity_id = 'identity_id'
-        mocked_audio.guid = 'someguid'
+    def test_delete(self, mock_get_audio, mock_media_delete,
+                    mock_delete, mock_commit):
+        mock_audio, mock_user = MagicMock(), MagicMock()
+        mock_user.identity_id = 'identity_id'
+        mock_audio.guid = 'someguid'
         audio_ids = [2]
-        mocked_get_audio.return_value = mocked_audio
+        mock_get_audio.return_value = mock_audio
 
-        result = audio_operation.delete(mocked_user, audio_ids)
+        result = audio_operation.delete(mock_user, audio_ids)
 
-        mocked_get_audio.assert_called_with(mocked_user, audio_ids[0])
-        mocked_media_delete.assert_called_with(
+        mock_get_audio.assert_called_with(mock_user, audio_ids[0])
+        mock_media_delete.assert_called_with(
             'identity_id/someguid', settings.S3_BUCKET_AUDIO)
-        mocked_delete.assert_called_with(mocked_audio)
-        mocked_commit.assert_called_with()
+        mock_delete.assert_called_with(mock_audio)
+        mock_commit.assert_called_with()
         self.assertTrue(result)
 
     @patch.object(models.db.session, 'commit')
     @patch.object(models.db.session, 'delete')
     @patch.object(media_storage, 'delete')
     @patch.object(audio_operation, 'get_audio_or_assert')
-    def test_delete_file_error(self, mocked_get_audio, mocked_media_delete,
-                               mocked_delete, mocked_commit):
-        mocked_user = MagicMock()
+    def test_delete_file_error(self, mock_get_audio, mock_media_delete,
+                               mock_delete, mock_commit):
+        mock_user = MagicMock()
         audio_ids = [2]
-        mocked_media_delete.side_effect = ValueError()
+        mock_media_delete.side_effect = ValueError()
 
-        audio_operation.delete(mocked_user, audio_ids)
+        audio_operation.delete(mock_user, audio_ids)
 
-        mocked_delete.assert_not_called()
-        mocked_commit.assert_called_with()
+        mock_delete.assert_not_called()
+        mock_commit.assert_called_with()
 
     @patch.object(audio_operation, 'get_audio_url')
     @patch('highland.models.Episode.query')
     @patch('highland.models.Audio.query')
     def test_load(
-            self, mocked_query_audio, mocked_query_episode, mocked_get_url):
-        mocked_user = MagicMock()
-        mocked_user.id = 1
+            self, mock_query_audio, mock_query_episode, mock_get_url):
+        mock_user = MagicMock()
+        mock_user.id = 1
 
-        mocked_filter_audio = MagicMock()
-        mocked_audio_01, mocked_audio_02, mocked_audio_03 = \
+        mock_filter_audio = MagicMock()
+        mock_audio_01, mock_audio_02, mock_audio_03 = \
             MagicMock(), MagicMock(), MagicMock()
-        mocked_audio_01.id = 11    # used
-        mocked_audio_02.id = 12    # used but whitelisted
-        mocked_audio_03.id = 13    # not used
-        mocked_audio_01.__iter__.return_value = iter({'id': 11})
-        mocked_audio_02.__iter__.return_value = iter({'id': 12})
-        mocked_audio_03.__iter__.return_value = iter({'id': 13})
-        mocked_filter_audio.all.return_value = [
-            mocked_audio_01, mocked_audio_02, mocked_audio_03]
-        mocked_query_audio.filter_by.return_value = mocked_filter_audio
+        mock_audio_01.id = 11    # used
+        mock_audio_02.id = 12    # used but whitelisted
+        mock_audio_03.id = 13    # not used
+        mock_audio_01.__iter__.return_value = iter({'id': 11})
+        mock_audio_02.__iter__.return_value = iter({'id': 12})
+        mock_audio_03.__iter__.return_value = iter({'id': 13})
+        mock_filter_audio.all.return_value = [
+            mock_audio_01, mock_audio_02, mock_audio_03]
+        mock_query_audio.filter_by.return_value = mock_filter_audio
 
-        mocked_filter_episode = MagicMock()
-        mocked_episode_01, mocked_episode_02, mocked_episode_03 = \
+        mock_filter_episode = MagicMock()
+        mock_episode_01, mock_episode_02, mock_episode_03 = \
             MagicMock(), MagicMock(), MagicMock()
-        mocked_episode_01.audio_id = 11    # used
-        mocked_episode_02.audio_id = 12    # used but whitelisted
-        mocked_episode_03.audio_id = None  # not used
-        mocked_episode_01.id = 21
-        mocked_episode_02.id = 22
-        mocked_episode_03.id = 23
-        mocked_filter_episode.all.return_value = [
-            mocked_episode_01, mocked_episode_02, mocked_episode_03]
-        mocked_query_episode.filter_by.return_value = mocked_filter_episode
+        mock_episode_01.audio_id = 11    # used
+        mock_episode_02.audio_id = 12    # used but whitelisted
+        mock_episode_03.audio_id = None  # not used
+        mock_episode_01.id = 21
+        mock_episode_02.id = 22
+        mock_episode_03.id = 23
+        mock_filter_episode.all.return_value = [
+            mock_episode_01, mock_episode_02, mock_episode_03]
+        mock_query_episode.filter_by.return_value = mock_filter_episode
 
-        result = audio_operation.load(mocked_user, True, 12)
+        result = audio_operation.load(mock_user, True, 12)
 
-        mocked_query_audio.filter_by.assert_called_with(
-            owner_user_id=mocked_user.id)
-        mocked_filter_audio.all.assert_called_with()
-        mocked_query_episode.filter_by.assert_called_with(
-            owner_user_id=mocked_user.id)
-        mocked_filter_episode.all.assert_called_with()
+        mock_query_audio.filter_by.assert_called_with(
+            owner_user_id=mock_user.id)
+        mock_filter_audio.all.assert_called_with()
+        mock_query_episode.filter_by.assert_called_with(
+            owner_user_id=mock_user.id)
+        mock_filter_episode.all.assert_called_with()
         self.assertEqual(2, len(result))
         self.assertEqual([22, None], [x['episode_id'] for x in result])
 
     @patch.object(audio_operation, 'access_allowed_or_raise')
     @patch('highland.models.Audio.query')
-    def test_get_audio_or_assert(self, mocked_query, mocked_access):
-        mocked_user, mocked_audio = MagicMock(), MagicMock()
-        mocked_filter = MagicMock()
-        mocked_filter.first.return_value = mocked_audio
-        mocked_query.filter_by.return_value = mocked_filter
+    def test_get_audio_or_assert(self, mock_query, mock_access):
+        mock_user, mock_audio = MagicMock(), MagicMock()
+        mock_filter = MagicMock()
+        mock_filter.first.return_value = mock_audio
+        mock_query.filter_by.return_value = mock_filter
 
         result = audio_operation.get_audio_or_assert(
-            mocked_user, mocked_audio.id)
+            mock_user, mock_audio.id)
 
-        mocked_query.filter_by.assert_called_with(
-            owner_user_id=mocked_user.id, id=mocked_audio.id)
-        mocked_filter.first.assert_called_with()
-        mocked_access.assert_called_with(mocked_user.id, mocked_audio)
-        self.assertEqual(result, mocked_audio)
+        mock_query.filter_by.assert_called_with(
+            owner_user_id=mock_user.id, id=mock_audio.id)
+        mock_filter.first.assert_called_with()
+        mock_access.assert_called_with(mock_user.id, mock_audio)
+        self.assertEqual(result, mock_audio)
 
     @patch.object(audio_operation, 'access_allowed_or_raise')
     @patch('highland.models.Audio.query')
-    def test_get_audio_or_assert_not_found(self, mocked_query, mocked_access):
-        mocked_filter = MagicMock()
-        mocked_filter.first.return_value = None
-        mocked_query.filter_by.return_value = mocked_filter
+    def test_get_audio_or_assert_not_found(self, mock_query, mock_access):
+        mock_filter = MagicMock()
+        mock_filter.first.return_value = None
+        mock_query.filter_by.return_value = mock_filter
 
         with self.assertRaises(exception.NoSuchEntityError):
             audio_operation.get_audio_or_assert(MagicMock(), 1)
-        mocked_access.assert_not_called()
+        mock_access.assert_not_called()
 
     @patch.object(audio_operation, 'access_allowed_or_raise')
-    def test_get_audio_url(self, mocked_access):
-        mocked_user, mocked_audio = MagicMock(), MagicMock()
-        mocked_user.identity_id = 'identity_id'
-        mocked_audio.guid = 'someguid'
+    def test_get_audio_url(self, mock_access):
+        mock_user, mock_audio = MagicMock(), MagicMock()
+        mock_user.identity_id = 'identity_id'
+        mock_audio.guid = 'someguid'
 
-        result = audio_operation.get_audio_url(mocked_user, mocked_audio)
+        result = audio_operation.get_audio_url(mock_user, mock_audio)
 
-        mocked_access.assert_called_with(mocked_user.id, mocked_audio)
+        mock_access.assert_called_with(mock_user.id, mock_audio)
         self.assertEqual(
             '{}/identity_id/someguid'.format(settings.HOST_AUDIO), result)
 
     def test_access_allowed_or_raise(self):
-        mocked_audio = MagicMock()
-        mocked_audio.owner_user_id = 1
-        result = audio_operation.access_allowed_or_raise(1, mocked_audio)
-        self.assertEqual(mocked_audio, result)
+        mock_audio = MagicMock()
+        mock_audio.owner_user_id = 1
+        result = audio_operation.access_allowed_or_raise(1, mock_audio)
+        self.assertEqual(mock_audio, result)
 
     def test_access_allowed_or_raise_raise(self):
-        mocked_audio = MagicMock()
-        mocked_audio.owner_user_id = 2
+        mock_audio = MagicMock()
+        mock_audio.owner_user_id = 2
         with self.assertRaises(exception.AccessNotAllowedError):
-            audio_operation.access_allowed_or_raise(1, mocked_audio)
+            audio_operation.access_allowed_or_raise(1, mock_audio)
--- tests/test_image_operation.py
+++ tests/test_image_operation.py
@@ -7,118 +7,110 @@ from highland import media_storage, image_operation, models, settings, \
 class TestImageOperation(unittest.TestCase):
     @patch.object(models.db.session, 'commit')
     @patch.object(models.db.session, 'add')
-    @patch('highland.models.Image')
     @patch('uuid.uuid4')
-    def test_create(
-            self, mocked_uuid4, mocked_image_class, mocked_add, mocked_commit):
-        mocked_user = MagicMock()
-        mocked_image = MagicMock()
-        mocked_image_class.return_value = mocked_image
+    def test_create(self, mock_uuid4, mock_add, mock_commit):
+        mock_uuid4.return_value.hex = 'some_guid'
 
-        mocked_uuid = MagicMock()
-        mocked_uuid4.return_value = mocked_uuid
+        result = image_operation.create(1, 'file.jpg', 'image/jpeg')
 
-        result = image_operation.create(mocked_user, 'file.jpg', 'image/jpeg')
-
-        mocked_add.assert_called_with(mocked_image)
-        mocked_commit.assert_called_with()
-        mocked_image_class.assert_called_with(
-            mocked_user, 'file.jpg', mocked_uuid.hex, 'image/jpeg')
-        self.assertEqual(mocked_image, result)
+        self.assertEqual(1, mock_add.call_count)
+        mock_commit.assert_called_with()
+        self.assertEqual(1, result.get('owner_user_id'))
+        self.assertEqual('some_guid', result.get('guid'))
 
     @patch.object(models.db.session, 'commit')
     @patch.object(models.db.session, 'delete')
     @patch.object(media_storage, 'delete')
     @patch.object(image_operation, 'get_image_or_assert')
-    def test_delete(self, mocked_get, mocked_storage_delete, mocked_db_delete,
-                    mocked_db_commit):
-        mocked_user, mocked_image_01, mocked_image_02 = \
+    def test_delete(self, mock_get, mock_storage_delete, mock_db_delete,
+                    mock_db_commit):
+        mock_user, mock_image_01, mock_image_02 = \
             MagicMock(), MagicMock(), MagicMock()
-        mocked_user.identity_id = 'identity_id'
-        mocked_image_02.guid = 'non-existing'
-        mocked_get.side_effect = \
-            lambda u, i: mocked_image_01 if i == 1 else mocked_image_02
+        mock_user.identity_id = 'identity_id'
+        mock_image_02.guid = 'non-existing'
+        mock_get.side_effect = \
+            lambda u, i: mock_image_01 if i == 1 else mock_image_02
 
         def f(key, bucket, folder=''):
             if key == 'identity_id/non-existing':
                 raise ValueError()
             else:
-                return mocked_image_01
-        mocked_storage_delete.side_effect = f
+                return mock_image_01
+        mock_storage_delete.side_effect = f
 
-        result = image_operation.delete(mocked_user, [1, 2])
+        result = image_operation.delete(mock_user, [1, 2])
 
-        self.assertEqual(2, mocked_get.call_count)
-        self.assertEqual(2, mocked_storage_delete.call_count)
-        self.assertEqual(1, mocked_db_delete.call_count)
-        mocked_db_delete.assert_called_with(mocked_image_01)
-        mocked_db_commit.assert_called_with()
+        self.assertEqual(2, mock_get.call_count)
+        self.assertEqual(2, mock_storage_delete.call_count)
+        self.assertEqual(1, mock_db_delete.call_count)
+        mock_db_delete.assert_called_with(mock_image_01)
+        mock_db_commit.assert_called_with()
         self.assertTrue(result)
 
     @patch('highland.models.Image.query')
-    def test_load(self, mocked_query):
-        mocked_user = MagicMock()
+    def test_load(self, mock_query):
+        mock_user = MagicMock()
         image_list = [MagicMock(), MagicMock()]
-        mocked_filter = MagicMock()
-        mocked_filter.all.return_value = image_list
-        mocked_query.filter_by.return_value = mocked_filter
+        mock_filter = MagicMock()
+        mock_filter.all.return_value = image_list
+        mock_query.filter_by.return_value = mock_filter
 
-        result = image_operation.load(mocked_user)
+        result = image_operation.load(mock_user)
 
-        mocked_query.filter_by.assert_called_with(owner_user_id=mocked_user.id)
-        mocked_filter.all.assert_called_with()
+        mock_query.filter_by.assert_called_with(owner_user_id=mock_user.id)
+        mock_filter.all.assert_called_with()
         self.assertEqual(image_list, result)
 
     @patch('highland.models.Image.query')
-    def test_get_image_or_assert(self, mocked_query):
-        mocked_user, mocked_image = MagicMock(), MagicMock()
-        mocked_image.owner_user_id = mocked_user.id
+    def test_get_image_or_assert(self, mock_query):
+        mock_user, mock_image = MagicMock(), MagicMock()
+        mock_image.owner_user_id = mock_user.id
 
-        mocked_filter = MagicMock()
-        mocked_filter.first.return_value = mocked_image
-        mocked_query.filter_by.return_value = mocked_filter
+        mock_filter = MagicMock()
+        mock_filter.first.return_value = mock_image
+        mock_query.filter_by.return_value = mock_filter
 
         result = image_operation.get_image_or_assert(
-            mocked_user, mocked_image.id)
+            mock_user, mock_image.id)
 
-        mocked_query.filter_by.assert_called_with(
-            owner_user_id=mocked_user.id, id=mocked_image.id)
-        mocked_filter.first.assert_called_with()
-        self.assertEqual(mocked_image, result)
+        mock_query.filter_by.assert_called_with(
+            owner_user_id=mock_user.id, id=mock_image.id)
+        mock_filter.first.assert_called_with()
+        self.assertEqual(mock_image, result)
 
     @patch('highland.models.Image.query')
-    def test_get_image_or_assert_assert(self, mocked_query):
-        mocked_user = MagicMock()
+    def test_get_image_or_assert_assert(self, mock_query):
+        mock_user = MagicMock()
 
-        mocked_filter = MagicMock()
-        mocked_filter.first.return_value = None
-        mocked_query.filter_by.return_value = mocked_filter
+        mock_filter = MagicMock()
+        mock_filter.first.return_value = None
+        mock_query.filter_by.return_value = mock_filter
 
         with self.assertRaises(exception.NoSuchEntityError):
-            image_operation.get_image_or_assert(mocked_user, 1)
+            image_operation.get_image_or_assert(mock_user, 1)
 
     @patch.object(image_operation, 'access_allowed_or_raise')
-    def test_get_image_url(self, mocked_access):
-        mocked_user, mocked_image = MagicMock(), MagicMock()
-        mocked_user.identity_id = 'identity_id'
-        mocked_image.guid = 'someguid'
+    def test_get_image_url(self, mock_access):
+        mock_user, mock_image = MagicMock(), MagicMock()
+        mock_user.identity_id = 'identity_id'
+        mock_image.guid = 'someguid'
 
-        result = image_operation.get_image_url(mocked_user, mocked_image)
+        result = image_operation.get_image_url(mock_user, mock_image)
 
-        mocked_access.assert_called_with(mocked_user.id, mocked_image)
+        mock_access.assert_called_with(mock_user.id, mock_image)
         self.assertEqual('{}/{}'.format(
             settings.HOST_IMAGE, 'identity_id/someguid'), result)
 
     def test_access_allowed_or_raise(self):
-        mocked_image = MagicMock()
-        mocked_image.owner_user_id = 1
+        mock_image = MagicMock()
+        mock_image.owner_user_id = 1
 
-        result = image_operation.access_allowed_or_raise(1, mocked_image)
+        result = image_operation.access_allowed_or_raise(1, mock_image)
 
-        self.assertEqual(mocked_image, result)
+        self.assertEqual(mock_image, result)
 
     def test_access_allowed_or_raise_raise(self):
-        mocked_image = MagicMock()
-        mocked_image.owner_user_id = 1
+        mock_image = MagicMock()
+        mock_image.owner_user_id = 1
         with self.assertRaises(exception.AccessNotAllowedError):
-            image_operation.access_allowed_or_raise(2, mocked_image)
+            image_operation.access_allowed_or_raise(2, mock_image)
